7f7adbdf6a6e30ca6b8c504645395d1bdecf8d05
Vigneshsubramanian7 <vigneshs@dckap.com>
2025-03-05 09:01:57 +0530

    Print Layout Corrections

--- src/Components/UI/PrintBill.jsx
+++ src/Components/UI/PrintBill.jsx
@@ -3,6 +3,7 @@ import { Download, Printer, Palette, SquareCheckIcon, Square } from "lucide-reac
 import { useRouter } from "next/router";
 import { ApiRequest } from "../Util/apiRequest";
 import GetNumberToWords from "../Util/numberToWords";
+import jsPDF from "jspdf";
 
 const defaultCompanyInfo = {
   name: "Bike Zone",
@@ -66,16 +67,16 @@ export function PrintBill() {
 
   return (
     <div className="min-h-screen bg-gray-50 print:bg-white">
-      <div className="print:hidden fixed top-0 left-0 right-0 bg-white shadow-sm z-50">
+      <div className="print:hidden fixed top-0 left-0 right-0 bg-white shadow-sm print:shadow-none z-50">
         <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
           <h1 className="text-xl ml-12 font-semibold text-green-600">Bill Number {billData.billNumber}</h1>
           <div className="flex items-center space-x-4">
             {/* <button
-              onClick={handleDownloadPDF}
+              onClick={generatePDF}
               className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-lg text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
             >
               <Download size={20} className="mr-2" />
-              Download PDF
+              Download 
             </button> */}
             <button
               onClick={handlePrint}
@@ -88,41 +89,41 @@ export function PrintBill() {
         </div>
       </div>
 
-      <div className="max-w-4xl mx-auto bg-white shadow-sm remove-margin my-20 p-8 print:my-0 print:shadow-none">
-        <div className="flex justify-between items-start mb-8">
+      <div className="max-w-4xl mx-auto bg-white shadow-sm remove-margin my-7 p-8 print:p-4 print:my-0 print:shadow-none">
+        <div className="flex justify-between items-start mb-8 print:mb-3">
           <div className="flex items-center space-x-4">
             {companyInfo.logo && <img src={companyInfo.logo} alt="Company Logo" className="w-16 h-16 object-contain" />}
             <div>
               <h2 className="text-2xl text-green-600 font-bold">{companyInfo.name}</h2>
-              <p className="text-gray-600">{companyInfo.address}</p>
-              <p className="text-gray-600">{companyInfo.city}</p>
+              <p className="text-black">{companyInfo.address}</p>
+              <p className="text-black">{companyInfo.city}</p>
             </div>
           </div>
           <div className="text-right">
             <h1 className="text-4xl text-green-600 font-bold mb-4">INVOICE</h1>
-            <p className="text-gray-600">Bill Number #{billData.billNumber}</p>
-            <p className="text-gray-600">Date: {new Date(billData.date).toLocaleDateString()}</p>
+            <p className="text-black">Bill Number #{billData.billNumber}</p>
+            <p className="text-black">Date: {new Date(billData.date).toLocaleDateString()}</p>
           </div>
         </div>
 
-        <div className="mb-8 flex justify-between items-center">
+        <div className="mb-10 print:mb-3 flex justify-between items-center">
           <div>
             <h3 className="text-lg text-green-600 font-semibold mb-2">Bill To:</h3>
             <div className="border-l-4 border-green-500 pl-4">
-              <p className="font-semibold text-gray-600">{billData.customer.name}</p>
-              <p className="text-gray-600">Customer Number: {billData.customer.number}</p>
-              <p className="text-gray-600">{billData.customer.address || ""}</p>
+              <p className="font-semibold text-black">{billData.customer.name}</p>
+              <p className="text-black">Customer Number: {billData.customer.number}</p>
+              <p className="text-black">{billData.customer.address || ""}</p>
             </div>
           </div>
           {billData.metaData.length && (
             <div>
               {billData.metaData.map((meta) =>
                 meta.dataType !== "Boolean" ? (
-                  <p className="text-gray-600">
+                  <p className="text-black">
                     {meta.label}: <span className="font-semibold"> {meta.value} </span>
                   </p>
                 ) : (
-                  <p className="text-gray-600 flex">
+                  <p className="text-black flex">
                     {meta.label}:{" "}
                     <span style={{ marginLeft: "7px" }} className="font-semibold block">
                       {" "}
@@ -135,7 +136,7 @@ export function PrintBill() {
           )}
         </div>
 
-        <table className="w-full mb-8">
+        <table className="w-full mb-8 print:mb-5">
           <thead>
             <tr className="text-left">
               <th className="py-2 font-semibold text-green-600">Item Description</th>
@@ -144,42 +145,43 @@ export function PrintBill() {
               <th className="py-2 font-semibold text-right text-green-600">Amount</th>
             </tr>
           </thead>
-          <tbody className="border-t border-b">
+          <tbody className="border-t border-b border-green-600">
             {computedItems.map((item, index) => (
-              <tr key={index} className="border-b text-gray-600 last:border-b-0">
-                <td className="py-3">{item.itemName}</td>
-                <td className="py-3 text-right">{item.itemQty}</td>
-                <td className="py-3 text-right">₹{Number(item.itemPrice).toFixed(2)}</td>
-                <td className="py-3 text-right">₹{item.total.toFixed(2)}</td>
+              <tr key={index} className=" text-black text-sm ">
+                <td className="py-2 print:py-1">{item.itemName}</td>
+                <td className="py-2 print:py-1 text-right">{item.itemQty}</td>
+                <td className="py-2 print:py-1 text-right">₹{Number(item.itemPrice).toFixed(2)}</td>
+                <td className="py-2 print:py-1 text-right">₹{item.total.toFixed(2)}</td>
               </tr>
             ))}
           </tbody>
         </table>
 
-        <div className="w-1/2 ml-auto space-y-2">
+        <div className="w-1/2 ml-auto space-y-1">
           <div className="flex justify-between">
-            <span className="text-gray-600">Subtotal:</span>
-            <span className="font-medium text-gray-600">₹{subtotal.toFixed(2)}</span>
+            <span className="text-black">Subtotal:</span>
+            <span className="font-medium text-black">₹{subtotal.toFixed(2)}</span>
           </div>
           <div className="flex justify-between">
-            <span className="text-gray-600">Tax ({billData.tax}%):</span>
-            <span className="font-medium text-gray-600">₹{taxAmount.toFixed(2)}</span>
+            <span className="text-black">Tax ({billData.tax}%):</span>
+            <span className="font-medium text-black">₹{taxAmount.toFixed(2)}</span>
           </div>
           <div className="flex justify-between">
-            <span className="text-gray-600">Discount ({billData.discount}%):</span>
-            <span className="font-medium text-gray-600">-₹{discountAmount.toFixed(2)}</span>
+            <span className="text-black">Discount ({billData.discount}%):</span>
+            <span className="font-medium text-black">-₹{discountAmount.toFixed(2)}</span>
           </div>
-          <div className="flex justify-between pt-2 text-gray-600 border-y font-bold text-lg">
+          <div className="flex justify-between pt-2 text-black border-y font-bold text-lg">
             <span>Total:</span>
             <span>₹{grandTotal.toFixed(2)}</span>
           </div>
         </div>
-        <div className="mt-5 text-gray-600">
+        <div className="mt-7 text-black">
           Total amount in words -{" "}
           <span className="font-extrabold text-green-600">{GetNumberToWords(grandTotal.toFixed(0))}</span>{" "}
         </div>
+        <div className="mt-5 print:mt-1.5 mr-10 text-black text-right">Signature</div>
 
-        <div className="mt-12 pt-4 border-t text-center text-gray-600">
+        <div className="mt-12 print:mt-5 pt-4 border-t text-center text-black">
           <p className="font-medium">{companyInfo.name}</p>
           <p>
             {companyInfo.number}
